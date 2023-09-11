abbf511b88e23fc4e109fd245414faf8e781dc54
irustarodrigo <141684717+irustarodrigo@users.noreply.github.com>
2023-09-11 19:24:12 -0300

    Aca creé el paquete Entidades, dentro de universidadAPP, ahi dentro creé las 3 clases, alumno le puse Ealumno porque habia un Alumno.java (que era una vista) y por las dudas que de error, despues creé Inscripcion y Materia.

---
+++ src/universidadAPP/Entidades/Inscripcion.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadAPP.Entidades;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class Inscripcion {
+    private int idInscripto;
+    private int nota;
+    private Ealumno alumno;
+    private Materia materia;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(int nota, Ealumno alumno, Materia materia) {
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public Inscripcion(int idInscripto, int nota, Ealumno alumno, Materia materia) {
+        this.idInscripto = idInscripto;
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public int getIdInscripto() {
+        return idInscripto;
+    }
+
+    public void setIdInscripto(int idInscripto) {
+        this.idInscripto = idInscripto;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public Ealumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Ealumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    @Override
+    public String toString() {
+        //aca traigo todo el alumno y toda la materia, se puede elegir lo que haga falta
+        return "Inscripcion{" + "idInscripto=" + idInscripto + ", nota=" + nota + ", alumno=" + alumno + ", materia=" + materia + '}';
+    }
+    
+    
+    
+}
